ef8dce5631f0b3013cacac139cffbc5bfced05aa
gabriel-chagas-albuquerque <gabrieldafortaleza0100@gmail.com>
2025-06-16 15:34:55 -0300

    Criação da tela com menu inicial e tela que mostra a fila de pacientes

--- scripts/fila-de-espera.js
+++ scripts/fila-de-espera.js
@@ -1,6 +1,19 @@
 const fila = JSON.parse(localStorage.getItem('filaDeAtendimento'))
 const filaDeEspera = document.querySelector('#filaDeEspera')
 const filaOrdenada = fila.sort((a,b) => b.prioridadeCode - a.prioridadeCode)
+let pacientes = ""
+filaDeEspera.innerHTML = filaOrdenada.map((paciente) => {
+        pacientes += `
+                        <li>
+                            <h4>${paciente.nome}</h4>
+                            <p>${paciente.prioridade}</p>
+                        </li>
+                        
+                    `
+    })
+
+
+filaDeEspera.innerHTML = pacientes
 
 
 
